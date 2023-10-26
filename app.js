c32eae7a0266c9e7e95de20e6505b7899ecbc9f1
Tim Bydlon <tim.bydlon@salesforce.com>
2023-10-26 17:41:30 -0400

    fixed bug and now random assignment is working

--- app.js
+++ app.js
@@ -453,10 +453,10 @@ async function getButtonResponse(success, messageId, channel)
 				responseObj.icon = slackResponse.fail_reaction;
 				// Replace auto-assign tag with a random contact assigned to the channel
 				if (responseObj.text.includes("{Random_Assign}")) {
-					const channel_contact_ids = await pool.query('SELECT slack_id__c FROM salesforce.Contact__c WHERE channels__c = ANY($1);', [channel.id]);
-					const randomIndex = Math.floor(Math.random() * channel_contact_ids.length);
-					const assigned_contact_id = channel_contact_ids[randomIndex]
-					responseObj.text = responseObj.text.replace("{Random_Assign}", assigned_contact_id)
+					const channel_contact_ids = await pool.query('SELECT slack_id__c FROM salesforce.Contact__c WHERE $1 = ANY(channels__c);', [channel.id]);
+					const randomIndex = Math.floor(Math.random() * channel_contact_ids.rows.length);
+					const assigned_contact_id = channel_contact_ids.rows[randomIndex].slack_id__c;
+					responseObj.text = responseObj.text.replace("{Random_Assign}", assigned_contact_id);
 				}
 			}
 
